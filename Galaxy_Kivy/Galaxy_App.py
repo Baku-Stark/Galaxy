02984ef5e95009c266fbfbd7d7a75128616e80c5
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-08 15:46:51 -0300

    🐍 Update: Galaxy Update

--- Galaxy_Kivy/Galaxy_App.py
+++ Galaxy_Kivy/Galaxy_App.py
@@ -27,12 +27,16 @@ class MainWidget(Widget):
         perspective_point_y : any
 
         line   : NoneType
+
+        V_LINES_SPACING : percentage in screen width
     """
 
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    line = None
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1
+    vertical_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -86,13 +90,24 @@ class MainWidget(Widget):
         with self.canvas:
             Color(1, 1, 1)
             
-            self.line = Line(
-                points=[100, 0, 100, 100]
-            )
+            # self.line = Line(
+            #     points=[100, 0, 100, 100]
+            # )
+
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
 
     def update_vertical_lines(self):
-        center_x = int(self.width / 2)
-        self.line.points = [center_x, 0, center_x, 100]
+        # self.line.points = [center_x, 0, center_x, 100]
+
+        center_line_x = int(self.width / 2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2)
+
+        for i in range(0, self.V_NB_LINES):
+                line_x = int(center_line_x + offset * spacing)
+                self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+                offset += 1
 
 class Galaxy(App):
     """
