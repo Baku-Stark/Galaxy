12fae78a49e410dc570e70e252ec4a3310b2c88f
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-09 17:05:43 -0300

    🐍 Update: Galaxy Update

--- Galaxy_Kivy/Galaxy_App.py
+++ Galaxy_Kivy/Galaxy_App.py
@@ -28,20 +28,31 @@ class MainWidget(Widget):
 
         line   : NoneType
 
-        V_LINES_SPACING : percentage in screen width
+        [LINHA VERTICAL]
+        V_NB_LINES      : quantidade de linhas
+        V_LINES_SPACING : espaço entre as linhas
+
+        [LINHA HORIZONTAL]
+        H_NB_LINES      : quantidade de linhas
+        H_LINES_SPACING : espaço entre as linhas
     """
 
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    V_NB_LINES = 7
+    V_NB_LINES = 10
     V_LINES_SPACING = .1
     vertical_lines = []
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = .2
+    horizontal_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print(f'INIT\nW: {self.width}\nH: {self.height}')
         self.init_vertical_lines()
+        self.init_horizontal_lines()
 
     def on_parent(self, widget, parent):
         # print(f'ON PARENT\nW: {self.width}\nH: {self.height}')
@@ -59,6 +70,7 @@ class MainWidget(Widget):
         # self.perspective_point_y = self.height * 0.75
 
         self.update_vertical_lines()
+        self.update_horizontal_lines()
 
     def on_perspective_point_x(self, widget, value):
         """
@@ -87,6 +99,10 @@ class MainWidget(Widget):
         print(f'PY: {str(value)}')
 
     def init_vertical_lines(self):
+        """
+            Gerar linhas na vertical.
+        """
+
         with self.canvas:
             Color(1, 1, 1)
             
@@ -97,22 +113,99 @@ class MainWidget(Widget):
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
+    def init_horizontal_lines(self):
+        """
+            Gerar linhas na horizontal.
+        """
+
+        with self.canvas:
+            Color(1, 1, 1)
+
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
     def update_vertical_lines(self):
+        """
+            Atualizar a posição para manter as linhas verticais centralizadas.
+        """
         # self.line.points = [center_x, 0, center_x, 100]
 
         center_line_x = int(self.width / 2)
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2)
+        offset = -int(self.V_NB_LINES/2) + 0.5
 
         for i in range(0, self.V_NB_LINES):
-                line_x = int(center_line_x + offset * spacing)
-                self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
-                offset += 1
+            line_x = int(center_line_x + offset * spacing)
+
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+
+            offset += 1
+
+    def update_horizontal_lines(self):
+        """
+            Atualizar a posição para manter as linhas horizontais centralizadas.
+        """
+
+        center_line_x = int(self.width / 2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = int(self.V_NB_LINES/2) - 0.5
+
+        xmin = center_line_x - offset * spacing
+        xmax = center_line_x + offset * spacing
+        spacing_y = self.H_LINES_SPACING * self.height
+
+
+        for i in range(0, self.H_NB_LINES):
+            line_y = i * self.H_LINES_SPACING * self.height
+
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
+    def transform(self, x, y):
+        """
+            Transformar as linhas para a perspectiva 2D/3D.
+        """
+
+        #return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+    
+    def transform_2D(self, x:int, y:int):
+        """
+            Transformar para 2D.
+        """
+
+        return x, y
+    
+    def transform_perspective(self, x:int, y:int):
+        """
+            Criar a perspetiva do cenário.
+        """
+
+        # TRANSFORM [Y]
+        tr_y = y * self.perspective_point_y / self.height
+
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y
+
+        # TRANSFORM [X]
+        tr_x = self.perspective_point_x + diff_x * proportion_y
+
+        return tr_x, tr_y
 
 class Galaxy(App):
     """
         GALAXY application main class
     """
+
     pass
 
 if __name__ == '__main__':
